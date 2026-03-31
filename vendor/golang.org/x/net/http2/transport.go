a2dbc85de800c3cdba3cdfd0c73b9b6c722c4292
Nicholas S. Husin <nsh@golang.org>
2026-03-31 15:15:30 -0400

    http2: prevent hanging Transport due to bad SETTINGS frame
    This CL backports https://go.dev/cl/761581 to x/net.
    
    Fixes golang/go#78476
    Fixes CVE-2026-33814
    
    Change-Id: Ied435a51fdd8664d41dae14d082c39c76a6a6964
    Reviewed-on: https://go-review.googlesource.com/c/net/+/761640
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Nicholas Husin <husin@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>

--- vendor/golang.org/x/net/http2/transport.go
+++ vendor/golang.org/x/net/http2/transport.go
@@ -3108,6 +3108,9 @@ func (rl *clientConnReadLoop) processSettingsNoWrite(f *SettingsFrame) error {
 
 	var seenMaxConcurrentStreams bool
 	err := f.ForeachSetting(func(s Setting) error {
+		if err := s.Valid(); err != nil {
+			return err
+		}
 		switch s.ID {
 		case SettingMaxFrameSize:
 			cc.maxFrameSize = s.Val
@@ -3139,9 +3142,6 @@ func (rl *clientConnReadLoop) processSettingsNoWrite(f *SettingsFrame) error {
 			cc.henc.SetMaxDynamicTableSize(s.Val)
 			cc.peerMaxHeaderTableSize = s.Val
 		case SettingEnableConnectProtocol:
-			if err := s.Valid(); err != nil {
-				return err
-			}
 			// If the peer wants to send us SETTINGS_ENABLE_CONNECT_PROTOCOL,
 			// we require that it do so in the first SETTINGS frame.
 			//
